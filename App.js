70caac62ca8c4da365f09011e9fc263726ef3d3c
yangyongqian <624931379@qq.com>
2018-03-30 18:32:09 +0800

    做一些修改

--- App.js
+++ App.js
@@ -32,21 +32,20 @@ const instructions = Platform.select({
   android: 'Double tap R on your keyboard to reload,\n' +
     'Shake or press menu button for dev menu',
 });
+import TabNavigator from 'react-native-tab-navigator';
 
 type Props = {}; 
 
 export default class App extends Component<Props> {
   render() {
+    let tabBarHeight = 0;
      return ( 
       <View style={styles.container}>
-        <SectionList    
-            sections = {[
-               {title: 'D', data: ['Devin']},
-               {title: 'J', data: ['Jackson', 'James', 'Jillian', 'Jimmy', 'Joel', 'John', 'Julie']},
-            ]}
-            renderItem = {({item}) => <Text style={styles.item}>{item}</Text>}
-            renderSectionHeader = {({section}) => <Text style={styles.sectionHeader}>{section.title}</Text>}
+            <TabNavigator
+              tabBarStyle={{ height: tabBarHeight, overflow: 'hidden' }}
+              sceneStyle={{ paddingBottom: tabBarHeight }}
         />   
+            {/* <Text>什么鬼</Text> */}
       </View>
      )
   }
@@ -56,20 +55,6 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     paddingTop: 22
-   },
-   sectionHeader: {
-     paddingTop: 2,
-     paddingLeft: 10,
-     paddingRight: 10,
-     paddingBottom: 2,
-     fontSize: 14,
-     fontWeight: 'bold',
-     backgroundColor: 'rgba(247,247,247,1.0)',
-   },
-   item: {
-     padding: 10,
-     fontSize: 18,
-     height: 44,
    },
 });
 
